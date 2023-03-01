e464fdba57163e0f697d33281603ed143a32d819
Alexander-Krokhmal <zeddicus.ak@gmail.com>
2023-03-01 23:21:44 +0200

    Add complete task

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { tryCatchWrapper, auth, upload } = require('../../middlewares');
-const { getCurrentUser, updateAvatar } = require("../../controllers");
+const { tryCatchWrapper, auth, upload, validation } = require('../../middlewares');
+const { getCurrentUser, updateAvatar, verifyEmail, reVerification } = require("../../controllers");
+const { userVerificationJoiSchema } = require('../../schemas/userSchema');
 
 const router = express.Router();
 
 router.get("/current", auth, tryCatchWrapper(getCurrentUser));
 router.patch("/avatars", auth, upload.single("avatar"), tryCatchWrapper(updateAvatar));
+router.get("/verify/:verificationToken", tryCatchWrapper(verifyEmail));
+router.post("/verify", validation(userVerificationJoiSchema), tryCatchWrapper(reVerification));
 
 module.exports = router;
